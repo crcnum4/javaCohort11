03d41411b25d5367f177442d3c8da3317db2911d
Cliff Choiniere <crcnum4@gmail.com>
2020-10-20 20:21:38 -0400

    bankaccount and checkingaccount

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello CareerDevs");
-        Car myCar = new Car("Blue", "Subaru", "1234", 2021 );
-        System.out.println(myCar.model);
-        myCar.model = "Roadster X";
-        System.out.println(myCar.model);
-        System.out.println(myCar.toString());
-        myCar.displayDetails();
+        BankAccount myAcct = new BankAccount(100000, "Phil", "1234", "Account");
+        CheckingAccount myChecking = new CheckingAccount(20000, "Chelsea", "9876", 10000);
+        System.out.println(myAcct.toString());
+        System.out.println(myChecking.toString());
+        myAcct.deposit(1000);
+        myChecking.deposit(2000);
+        System.out.println(myAcct.toString());
+        System.out.println(myChecking.toString());
+        myAcct.withdraw(10000);
+        myChecking.withdraw(5000);
+        System.out.println(myAcct.toString());
+        System.out.println(myChecking.toString());
+        myAcct.withdraw(100000000);
+        myChecking.withdraw(6000);
+        myChecking.withdraw(4000);
+        System.out.println(myAcct.toString());
+        System.out.println(myChecking.toString());
     }
 }
