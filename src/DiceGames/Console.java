df3302fd824f73ac6ffdb60f854b0d5e0ade1b82
Cliff Choiniere <crcnum4@gmail.com>
2021-01-05 19:55:31 -0500

    liars dice and Console class challenge

--- src/DiceGames/Console.java
+++ src/DiceGames/Console.java
@@ -1,33 +1,15 @@
 package DiceGames;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Console {
-    static Scanner scanner = new Scanner(System.in);
-
-    static public void welcome() {
-        System.out.println("Welcome to Yahtzee");
-    }
-
-    static public int getChoice() {
-        System.out.print("pick a die 1 - 5 to re-roll: ");
-        int choice = scanner.nextInt();
-        return choice - 1;
-
-    }
-
-    static public List<Integer> getChoices() {
-        List<Integer> choices = new ArrayList<>();
-        // TODO: get the choices
-        System.out.println("Which dice do you want to reroll?");
-        String input = scanner.nextLine(); // "2 4 5"
-        String[] inputArray = input.split(" "); // ["2", "4", "5"] -> <2, 4, 5>
-        for (String number : inputArray) {
-            choices.add(Integer.parseInt(number) - 1);
-        }
-
-        return choices;
+    public int getInt(int min, int max, String query) {
+        return 0;
     }
 }
+
+//getInt(1, 4, "i want a number")
+
+//output
+// i want number
+// 0 reask
+// 5 reask
+// 3 return 3
---
+++ src/DiceGames/YahtzeeConsole.java
@@ -0,0 +1,33 @@
+package DiceGames;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class YahtzeeConsole {
+    static Scanner scanner = new Scanner(System.in);
+
+    static public void welcome() {
+        System.out.println("Welcome to Yahtzee");
+    }
+
+    static public int getChoice() {
+        System.out.print("pick a die 1 - 5 to re-roll: ");
+        int choice = scanner.nextInt();
+        return choice - 1;
+
+    }
+
+    static public List<Integer> getChoices() {
+        List<Integer> choices = new ArrayList<>();
+        // TODO: get the choices
+        System.out.println("Which dice do you want to reroll?");
+        String input = scanner.nextLine(); // "2 4 5"
+        String[] inputArray = input.split(" "); // ["2", "4", "5"] -> <2, 4, 5>
+        for (String number : inputArray) {
+            choices.add(Integer.parseInt(number) - 1);
+        }
+
+        return choices;
+    }
+}
